aee40e8767770ca8ed2e0e6a15d6a0e6ceb70c7e
leonardovilde <leonardovilde@gmail.com>
2025-07-03 11:56:30 -0300

    exercio aluno

--- assets/js/form-validaçao.js
+++ assets/js/form-validaçao.js
@@ -28,8 +28,42 @@ function limparCampos(){
 
     let inputEstrangeiro = document.querySelector("#campo-estrangeiro");
     inputEstrangeiro.checked = false;
+}
+
+function calcularMedia(){
+    let inputNota1 = document.querySelector("#campo-nota1")
+    let inputNota2 = document.querySelector("#campo-nota2")
+    let inputNota3 = document.querySelector("#campo-nota3")
+
+    let nota1 = parseFloat(inputNota1.value);
+    let nota2 = parseFloat(inputNota2.value);
+    let nota3 = parseFloat(inputNota3.value);
+
+    let media = (nota1 + nota2 + nota3) / 3;
+    return media;
+}
+
+function preencherCampoMedia(){
+    let spanMedia = document.querySelector("#media-valor");
+
+    let media = calcularMedia();
+
+    if(isNaN(media) ===true){
+        // Return sem nada depois n faz o metodo ser com retorno, simplesmente 
+        // encerra a execução deste método
+        return;
+    }
+
+    let mediaFormatada = media.toLocaleString('pt-BR', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    })
+
+    spanMedia.innerHTML = mediaFormatada;
 
 }
+
+
 function cadastrar(){
     let inputNome = document.querySelector("#campo-nome");
     let nome = inputNome.value;
@@ -48,19 +82,34 @@ function cadastrar(){
         alert("escolha uma modalidade");
         return ; // Encerra a execução da função
     }
-    let modalidade = campoModalidade.value
+    let modalidade = campoModalidade.value;
 
-    let campoEstrangeiro = document.querySelector("#campo-estrangeiro")
+    let campoEstrangeiro = document.querySelector("#campo-estrangeiro");
     let estrangeiro = campoEstrangeiro.checked;
 
-    
+    let media = calcularMedia();
+    let statusMedia = "";
+    if (media >= 6.00){
+        statusMedia = "Aprovado";
+    }else{
+        statusMedia = "Reprovado";
+    }
 
-   alert(
-    "\nNome: " +nome +
-    "\nSobrenome: " +sobrenome +
+    let textarea = document.querySelector("#campo-alunos");
+    // Fluxo para acumular dados em uma string
+    // variavel = variavel + novo
+    // let texto = "Abacate";
+    // texto = texto + "Pera"; // Abacate Pera
+    // texto = texto +"Uva"; // Abacate Pera Uva
+    textarea.value = textarea.value +"\nNome: " + nome +
+    "\nSobrenome: " + sobrenome +
+    "\nMédia:" + media +
+    "\nStatus:" + statusMedia +
     "\nIdade: " + idade +
     "\nClasse: " + classe +
     "\nModalidade:" + modalidade +
-    "\nEstrangeiro: " + estrangeiro
-   );
+    "\nEstrangeiro: " + estrangeiro +
+    "\n-----------------------------------------\n";
+
+    alert ("cadastro realizado com sucesso");
 }
